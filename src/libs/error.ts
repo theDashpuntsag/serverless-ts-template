b8b7585dfa98fda1d89b83d6de0443d7215e5592
thedashpuntsag <thedashpuntsag@gmail.com>
2024-12-11 11:14:03 +0800

    Updated error handling function

--- src/libs/error.ts
+++ src/libs/error.ts
@@ -1,28 +1,28 @@
-import { APIGatewayProxyResultV2 } from 'aws-lambda';
-import logger from './winston';
+import type { APIGatewayProxyResultV2 } from 'aws-lambda';
+import type { LbdFuncResponse } from '@type/util.types';
 import { formatJSONApiResponse, formatJSONResponse } from './format';
 import axios, { AxiosError } from 'axios';
+import logger from './winston';
 import CustomError from '@configs/custom-error';
-import { LbdFuncResponse } from '@type/util.types';
 
 function handleApiFuncError(error: unknown): APIGatewayProxyResultV2 {
-  if (error instanceof Error) logger.error(`API Error occurred: ${error.message}`);
+  if (error instanceof Error) logger.error(`${error.message}`);
   if (axios.isAxiosError(error)) return handleAxiosApiError(error);
-  if (error instanceof Error) return formatJSONApiResponse({ error: { message: 'Generic error!' } }, 400);
-  if (error instanceof TypeError) return formatJSONApiResponse({ error: { message: 'Type error!' } }, 400);
-  if (error instanceof ReferenceError) return formatJSONApiResponse({ error: { message: 'Reference error!' } }, 400);
   if (error instanceof CustomError)
     return formatJSONApiResponse({ error: { message: error.message } }, error.statusCode);
+  if (error instanceof TypeError) return formatJSONApiResponse({ error: { message: 'Type error!' } }, 400);
+  if (error instanceof ReferenceError) return formatJSONApiResponse({ error: { message: 'Reference error!' } }, 400);
+  if (error instanceof Error) return formatJSONApiResponse({ error: { message: 'Generic error!' } }, 400);
   return formatJSONApiResponse({ error: { message: 'Unexpected error occurred' } }, 500);
 }
 
 function handleDefaultError(error: unknown): LbdFuncResponse {
   if (error instanceof Error) logger.error(`Function Error occurred: ${error.message}`);
   if (axios.isAxiosError(error)) return handleAxiosFuncError(error);
-  if (error instanceof Error) return formatJSONResponse({ error: { message: 'Generic error!' } }, 400);
+  if (error instanceof CustomError) return formatJSONResponse({ error: { message: error.message } }, error.statusCode);
   if (error instanceof TypeError) return formatJSONResponse({ error: { message: 'Type error!' } }, 400);
   if (error instanceof ReferenceError) return formatJSONResponse({ error: { message: 'Reference error!' } }, 400);
-  if (error instanceof CustomError) return formatJSONResponse({ error: { message: error.message } }, error.statusCode);
+  if (error instanceof Error) return formatJSONResponse({ error: { message: 'Generic error!' } }, 400);
   return formatJSONResponse({ error: { message: 'Unexpected error occurred' } }, 500);
 }
 
