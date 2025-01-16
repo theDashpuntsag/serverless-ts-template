a23fd2303b7d3b973251bc5579b161d1f698ac65
thedashpuntsag <thedashpuntsag@gmail.com>
2025-01-16 13:13:19 +0800

    optimized error log

--- src/error/error-handling.ts
+++ src/error/error-handling.ts
@@ -1,52 +1,69 @@
 import type { APIGatewayProxyResultV2 } from 'aws-lambda';
-import type { LbdFuncResponse } from '@/types/util.types';
 
-import axios, { AxiosError } from 'axios';
-import CustomError from '@/error/custom-error';
-import { logger } from '@/libs/winston';
 import { ZodError } from 'zod';
 import { formatApiResponse, formatResponse } from '@/libs';
+import axios, { AxiosError } from 'axios';
+import CustomError from './custom-error';
+import { LbdFuncResponse } from '@/types';
+import { logger } from '@/libs';
 
-function handleApiFuncError(error: unknown): APIGatewayProxyResultV2 {
-  if (error instanceof Error) logger.error(`${error.message}`);
-  if (axios.isAxiosError(error)) return handleAxiosApiError(error);
-  if (error instanceof CustomError) return formatApiResponse({ error: { message: error.message } }, error.statusCode);
-  if (error instanceof ZodError) return handleZodError(error);
-  return formatApiResponse({ error: { message: 'Unexpected error occurred' } }, 500);
+export function logErrorMessage(error: unknown, func: string = 'Error'): void {
+  if (axios.isAxiosError(error)) {
+    logger.error(`Axios err ${func}: ${error.message}`);
+    return;
+  }
+  if (error instanceof CustomError) {
+    logger.error(`Custom error ${func}: ${error.message}`);
+    return;
+  }
+  if (error instanceof ZodError) {
+    logger.error(`Zod error ${func}: ${error.message}`);
+    return;
+  }
+  if (error instanceof Error) {
+    logger.error(`Error ${func}: ${JSON.stringify(error.message)}`);
+    return;
+  }
 }
 
-function handleDefaultError(error: unknown): LbdFuncResponse {
-  if (error instanceof Error) logger.error(`Function Error occurred: ${error.message}`);
+export function handleApiFuncError(error: unknown): APIGatewayProxyResultV2 {
+  if (error instanceof Error) logger.error(`Error occurred!: ${JSON.stringify(error.message)}`);
+  if (axios.isAxiosError(error)) return handleAxiosApiError(error);
+  if (error instanceof CustomError) return formatApiResponse({ message: error.message }, error.statusCode);
+  if (error instanceof ZodError) return handleZodError(error);
+  return formatApiResponse({ message: 'Unexpected error occurred' }, 500);
+}
+
+export function handleDefaultError(error: unknown): LbdFuncResponse {
+  if (error instanceof Error) logger.error(`Error occurred!: ${JSON.stringify(error.message)}`);
   if (axios.isAxiosError(error)) return handleAxiosFuncError(error);
-  if (error instanceof CustomError) return formatResponse({ error: { message: error.message } }, error.statusCode);
+  if (error instanceof CustomError) return formatResponse({ message: error.message }, error.statusCode);
   if (error instanceof ZodError) return handleZodFuncError(error);
-  return formatResponse({ error: { message: 'Unexpected error occurred' } }, 500);
+  return formatResponse({ message: 'Unexpected error occurred' }, 500);
 }
 
 function handleZodError(error: ZodError): APIGatewayProxyResultV2 {
   const missingFields = error.errors.map((err) => err.path.join('.') || 'unknown field');
   const formattedMessage = `Missing or invalid fields: ${missingFields.join(', ')}`;
-  return formatApiResponse({ error: { message: formattedMessage } }, 400);
+  return formatApiResponse({ message: formattedMessage }, 400);
 }
 
 function handleZodFuncError(error: ZodError): LbdFuncResponse {
   const missingFields = error.errors.map((err) => err.path.join('.') || 'unknown field');
   const formattedMessage = `Missing or invalid fields: ${missingFields.join(', ')}`;
-  return formatResponse({ error: { message: formattedMessage } }, 400);
+  return formatResponse({ message: formattedMessage }, 400);
 }
 
 function handleAxiosApiError(error: AxiosError): APIGatewayProxyResultV2 {
   logger.error('Axios error in API function:', error);
   const status = error.response?.status || 500;
-  const errorData = error.response?.data || { message: 'Unknown API error' };
-  return formatApiResponse({ error: errorData }, status);
+  const message = error.response?.data || { message: 'Unknown API error' };
+  return formatApiResponse({ message }, status);
 }
 
 const handleAxiosFuncError = (error: AxiosError): LbdFuncResponse => {
   logger.error('Axios error in function:', error);
   const status = error.response?.status || 500;
-  const errorData = error.response?.data || { message: 'Unknown function error' };
-  return formatResponse({ error: errorData }, status);
+  const message = error.response?.data || 'Unknown function error';
+  return formatResponse({ message }, status);
 };
-
-export { handleApiFuncError, handleDefaultError };
