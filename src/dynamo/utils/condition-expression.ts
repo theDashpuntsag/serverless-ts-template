1f5f06a1d0a57b23a2089682d69d7529ca7e242f
thedashpuntsag <thedashpuntsag@gmail.com>
2025-01-31 08:29:06 +0800

    added generateConditionExp function

---
+++ src/dynamo/utils/condition-expression.ts
@@ -0,0 +1,29 @@
+/**
+ * Generates the conditional expression and attribute values for a DynamoDB update.
+ *
+ * @param statuses - Array of valid statuses
+ * @returns - An object containing the condition expression and attribute values
+ */
+export function generateConditionExpression(statuses: string[]): { condition: string; extra: Record<string, string> } {
+  if (!statuses.length) throw new Error('statusArr cannot be empty');
+
+  if (statuses.length === 1) {
+    return {
+      condition: `#status = :status0`,
+      extra: { ':status0': statuses[0] },
+    };
+  }
+
+  const conditions = statuses.map((_, index) => `:status${index}`).join(', ');
+  const conditionExpression = `#status IN (${conditions})`;
+
+  const expressionAttributeValues = statuses.reduce(
+    (acc, status, index) => {
+      acc[`:status${index}`] = status;
+      return acc;
+    },
+    {} as Record<string, string>
+  );
+
+  return { condition: conditionExpression, extra: expressionAttributeValues };
+}
--- src/dynamo/utils/index.ts
+++ src/dynamo/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './condition-expression';
 export * from './expression-attribute';
 export * from './key-condition';
 export * from './parsing';
